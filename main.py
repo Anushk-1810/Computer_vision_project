00b557235bc890540b1c1e4d6f86804098efc00f
Anushk-1810 <anushk.angdekar@mitaoe.ac.in>
2024-12-16 02:00:42 +0530

    Add dynamic model downloading from Google Drive

--- main.py
+++ main.py
@@ -111,9 +111,14 @@ import gdown
 
 def download_model():
     model_path = 'models/best.pt'
-    url = 'https://drive.google.com/file/d/1DC2kCygbBWUKheQ_9cFziCsYVSRw6axK/view?usp=drive_link'
-    gdown.download(url, model_path, quiet=False, fuzzy=True)
+    
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
 
+    url = 'https://drive.google.com/uc?id=1DC2kCygbBWUKheQ_9cFziCsYVSRw6axK'  # Correct Google Drive URL
+
+    # Download the model using gdown
+    gdown.download(url, model_path, quiet=False, fuzzy=True)
 
 
 
